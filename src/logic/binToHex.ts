e29bc4be4e093d5030a51eac7b6960c680efba7b
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-11 03:22:05 +0000

    fix bin -> hex

--- src/logic/binToHex.ts
+++ src/logic/binToHex.ts
@@ -1,11 +1,8 @@
 import { colors } from "../colors";
 
-export function binToHex(bin: string) {
-  const steps = [];
-
-  const groups = [];
-
+function parseToHex(bin: string, steps: string[]) {
   const rev = bin.split("").reverse();
+  const groups = [];
 
   for (let i = 0; i < rev.length; i += 4) {
     const group = rev
@@ -13,17 +10,52 @@ export function binToHex(bin: string) {
       .reverse()
       .join("");
 
-    groups.push(colors.magentaBright("0".repeat(4 - group.length)) + group);
+    groups.push(group);
   }
 
   let result = "";
   groups.reverse().forEach((group) => {
+    console.log(group);
     const r = parseInt(group, 2).toString(16);
     result += r;
-    steps.push(`${group} = ${r}`);
+    steps.push(
+      `${colors.magentaBright("0".repeat(4 - group.length)) + group} = ${r}`
+    );
   });
 
-  steps.push(colors.greenBright(`Computed, hex = ${result}`));
+  return result;
+}
+
+export function binToHex(bin: string) {
+  const steps: string[] = [];
+
+  const groups = [];
+
+  const [int, decimal] = bin.split(".");
+
+  let rInt, rDec;
+  steps.push("", colors.bold(`** Computing int ${int}`), "");
+  steps.push(
+    colors.greenBright(`Computed, hex = ${(rInt = parseToHex(int, steps))}`)
+  );
+
+  if (decimal) {
+    steps.push("", colors.bold(`** Computing float ${decimal}`), "");
+    steps.push(
+      colors.greenBright(
+        `Computed, hex = ${(rDec = parseToHex(decimal, steps))}`
+      )
+    );
+  }
+
+  steps.push("==========================================");
+  steps.push(
+    colors.cyan(
+      `All done converted ${colors.magentaBright(
+        bin + ""
+      )} to hex ${colors.magentaBright(`${rInt}.${rDec ?? 0}`)}`
+    )
+  );
 
   return steps;
 }
