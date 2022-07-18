897ddd276f7c46181414b4df16f3c7ef2bfb41cf
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-18 18:20:27 +0700

    Update hexToBin.ts

--- src/logic/hexToBin.ts
+++ src/logic/hexToBin.ts
@@ -7,11 +7,12 @@ export function hexToBin(hex: string) {
   for (let i = 0; i < hex.length; i++) {
     const hexChar = hex[i];
     const binChar = parseInt(hexChar, 16).toString(2);
-    bin += binChar;
+    bin += "0".repeat(4 - binChar.length) + binChar;
     steps.push(`hex[${i}]: ${hexChar} = ${binChar}`);
   }
 
   steps.push(colors.greenBright(`Computed, bin = ${bin}`));
+  steps.push(colors.cyan(`Computed by C++: `) + colors.magentaBright(hex) + " = " + colors.magentaBright(parseInt(hex, 16).toString(2)));
 
   return steps;
 }
